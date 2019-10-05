6cb66869127d82a118c10bebd2e265442f412dbc
carlota_barbosa <carlota_azevedo@hotmail.com>
2019-10-05 19:36:42 +0200

    first try

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,18 @@ public class Main {
     public static void main(String[] args ) {
 
 
-        for( int indx = 0; indx <101; indx +=2 ) {
-            System.out.println(indx);
+        for( int i = 0; i <101; i ++ ) {
+
+            if ( i == ){
+            System.out.println(i);
+
+            else if (i == 69 ){
+                    System.out.println(i);
+                }
+
+                }
         }
+
 
 
     }
