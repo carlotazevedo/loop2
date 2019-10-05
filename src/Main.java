f68ae13bf5f0a7595de4fbdbc1c1fbb17f4e5ea5
carlota_barbosa <carlota_azevedo@hotmail.com>
2019-10-05 19:14:01 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args ) {
+
+
+        for( int indx = 0; indx <101; indx +=2 ) {
+            System.out.println(indx);
+        }
+
+
+    }
+}
